8d8e67df6c39df2d83bcf1284cf69abe76bac426
Dhyana-J <magho909@gmail.com>
2021-01-29 20:06:10 +0900

    Update index.html, main.js, main.css
    media query completed

--- Github-example/main.js
+++ Github-example/main.js
@@ -9,10 +9,24 @@
         toggleElements();
     });
 
+    window.addEventListener('resize',function(){
+        if(window.innerWidth>1024){
+            offElements();
+        }
+    })
+
     function toggleElements(){
         //NodeList는 유사배열이므로, 반복 불가하다. 그래서 반복할 수 있게끔 만들어줘야한다.
         [].forEach.call($toggles,function(toggle){
             toggle.classList.toggle('on');
         });
     }
+
+    function offElements(){
+        [].forEach.call($toggles,function(toggle){
+            toggle.classList.remove('on');
+        });
+    }
+
+
 })(window,document)
